b0d954cea912502e5a6e14b9d3d26eca6588f367
s0me0ne-coder <tolgaucar44@gmail.com>
2023-09-21 12:45:36 +0300

    initial commit

--- src/main/java/plugin/Listeners.java
+++ src/main/java/plugin/Listeners.java
@@ -3,20 +3,21 @@
 package plugin;
 
 import org.bukkit.Bukkit;
+import org.bukkit.GameMode;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerMoveEvent;
 
-import me.barny1094875.utilitiesog.Utilities_OG;
+import com.destroystokyo.paper.event.player.PlayerAdvancementCriterionGrantEvent;
 
 public class Listeners implements Listener {
 
 	@EventHandler(priority = EventPriority.HIGHEST)
-	public void onPlayerMove(PlayerMoveEvent event) {
-
-		Utilities_OG utilities = (Utilities_OG) Bukkit.getPluginManager().getPlugin("Utilities-OG");
-
+	public void onAdvancementProgress(PlayerAdvancementCriterionGrantEvent event) {
+		if(event.getPlayer().getGameMode() != GameMode.SURVIVAL) {
+			event.setCancelled(true);
+		}
 	}
 
 }
